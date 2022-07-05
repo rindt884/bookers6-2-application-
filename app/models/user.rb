59f9f47755a0b2d747d3b6311d5d9e939fbc3a99
EC2 Default User <ec2-user@ip-172-31-35-136.us-west-2.compute.internal>
2022-07-05 14:13:56 +0000

    Added comment function

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   has_many :favorites, dependent: :destroy
-
+  has_many :book_comments, dependent: :destroy
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
