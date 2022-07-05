bd6c426365a30d8b89e4579a0c9f5527372ce119
EC2 Default User <ec2-user@ip-172-31-35-136.us-west-2.compute.internal>
2022-07-05 03:16:58 +0000

    Added favorite function

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   validates :introduction, length: { maximum: 50 }
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  has_many :favorites, dependent: :destroy
 
-  
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
 end
