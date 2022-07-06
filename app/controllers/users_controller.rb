f05cc22d1a5dfd74af032d0cc998c99bb8fb2053
EC2 Default User <ec2-user@ip-172-31-35-136.us-west-2.compute.internal>
2022-07-06 15:05:07 +0000

    Follow / follower function added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -13,12 +13,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to user_path(current_user)
-    end
   end
 
   def update
